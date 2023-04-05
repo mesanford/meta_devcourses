1f1f92a9021809538c3ca7856bf684e3dd7e446e
mesanford <129674027+mesanford@users.noreply.github.com>
2023-04-05 11:42:55 -0500

    js test1

---
+++ javascript/js-examples-1.js
@@ -0,0 +1,19 @@
+for (var i = 1; i <=10; i++) {
+    if (i == 1) {console.log("Gold Medal")}
+    else if (i == 2 ) {console.log("Silver Medal")}
+    else if (i == 3 ) {console.log("Bronze Medal")}
+    else {console.log(i)}
+}
+
+for (var i = 1; i <= 10; i++) {
+    if(i == 1) {
+        console.log("Gold medal")
+    } else if (i == 2) {
+        console.log("Silver medal")
+    } else if (i == 3) {
+        console.log("Bronze medal")
+    } else {
+        //this block will run if no condition matches
+        console.log(i)
+    }
+}
